6f8d95a137846752d4918b17a6d0e7a859d003e8
aoitaku <aoitaku@gmail.com>
2015-02-15 05:31:58 +0900

    refactor layout logic

--- lib/dxruby_sprite_ui/components/container_box.rb
+++ lib/dxruby_sprite_ui/components/container_box.rb
@@ -28,13 +28,10 @@ class Quincite::UI::ContainerBox < DXRuby::SpriteUI::Container
       &flow_slice(padding, 0, max_width)
     ).inject(0) {|height, row|
       component = row.max_by(&:layout_height)
+      next height if component.position == :absolute
       v_space = [v_margin, component.margin].max + height
-      if component.position == :absolute
-        height
-      else
-        v_margin = component.margin
-        v_space + component.height
-      end
+      v_margin = component.margin
+      v_space + component.height
     } + [v_margin, padding].max
   end
 
@@ -49,27 +46,21 @@ class Quincite::UI::ContainerBox < DXRuby::SpriteUI::Container
       v_space = [v_margin, component.margin].max + height
       v_margin = component.margin
       h_margin = padding
-      row.inject(0) {|width, component|
+      row.inject(0) do |width, component|
         h_space = [h_margin, component.margin].max + width
-        component.move(
-          self.x + h_space,
-          self.y + v_space + (max_component_height - component.height) / 2
-        )
-        if component.position == :absolute
-          width
-        else
-          h_margin = component.margin
-          h_space + component.width
-        end
-      }
+        component.move(self.x + h_space, self.y + v_space + (max_component_height - component.height) / 2)
+        next width if component.position == :absolute
+        h_margin = component.margin
+        h_space + component.width
+      end
       v_space + max_component_height
     }
   end
 
   def flow_slice(h_margin, width, max_width)
     -> component {
-      h_space = [h_margin, component.margin].max + component.width
       next false if component.position == :absolute
+      h_space = [h_margin, component.margin].max + component.width
       expected_width = width + component.layout_width + padding * 2
       if width > 0 and expected_width > max_width
         width = h_space
@@ -89,32 +80,23 @@ class Quincite::UI::ContainerBox < DXRuby::SpriteUI::Container
     component = components.max_by(&:layout_width)
     @computed_width = component.width + [component.margin, padding].max * 2
     @computed_height = components.inject(0) {|height, component|
-      v_space = [v_margin, component.margin].max + height
       component.resize(@computed_width, nil, padding)
-      if component.position == :absolute
-        height
-      else
-        v_margin = component.margin 
-        v_space + component.height
-      end
+      next height if component.position == :absolute
+      v_space = [v_margin, component.margin].max + height
+      v_margin = component.margin 
+      v_space + component.height
     } + [v_margin, padding].max
   end
 
   def vertical_box_move
     v_margin = padding
-    components.inject(0) {|height, component|
+    components.inject(0) do |height, component|
       v_space = [v_margin, component.margin].max + height
-      component.move(
-        self.x + [padding, component.margin].max,
-        self.y + v_space
-      )
-      if component.position == :absolute
-        height
-      else
-        v_margin = component.margin 
-        v_space + component.height
-      end
-    }
+      component.move(self.x + [padding, component.margin].max, self.y + v_space)
+      next height if component.position == :absolute
+      v_margin = component.margin 
+      v_space + component.height
+    end
   end
 
   def horizontal_box_resize
@@ -122,32 +104,23 @@ class Quincite::UI::ContainerBox < DXRuby::SpriteUI::Container
     component = components.max_by(&:layout_width)
     @computed_height = component.height + [component.margin, padding].max * 2
     @computed_width = components.inject(0) {|width, component|
-      h_space = [h_margin, component.margin].max + width
       component.resize(nil, @computed_height, padding)
-      if component.position == :absolute
-        width
-      else
-        h_margin = component.margin
-        h_space + component.width
-      end
+      next width if component.position == :absolute
+      h_space = [h_margin, component.margin].max + width
+      h_margin = component.margin
+      h_space + component.width
     } + [h_margin, padding].max
   end
 
   def horizontal_box_move
     h_margin = padding
-    components.inject(0) {|width, component|
+    components.inject(0) do |width, component|
       h_space = [h_margin, component.margin].max + width
-      component.move(
-        self.x + h_space,
-        self.y + (self.height - component.height) / 2
-      )
-      if component.position == :absolute
-        width
-      else
-        h_margin = component.margin
-        h_space + component.width
-      end
-    }
+      component.move(self.x + h_space, self.y + (self.height - component.height) / 2)
+      next width if component.position == :absolute
+      h_margin = component.margin
+      h_space + component.width
+    end
   end
 
 end
