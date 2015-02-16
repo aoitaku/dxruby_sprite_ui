4f0faf2ab81cd2b55f14dcace24f0d934c89bdbe
aoitaku <aoitaku@gmail.com>
2015-02-17 01:19:05 +0900

    add comment

--- lib/dxruby_sprite_ui/components/text_button.rb
+++ lib/dxruby_sprite_ui/components/text_button.rb
@@ -1,7 +1,32 @@
+################################################################################
+#
+# TextButton
+#
+# Author:  aoitaku
+# Licence: zlib/libpng
+#
+################################################################################
+
+
+
+################################################################################
+#
+# TextButton クラス.
+#
+# マウスイベントを受け付けることが可能な TextLabel.
+#
 class Quincite::UI::TextButton < Quincite::UI::TextLabel
 
+  # color プロパティ.
+  # $TODO: style に分離すべき
   attr_accessor :disable_color, :active_color, :hover_color
 
+  ##############################################################################
+  #
+  # インスタンスの初期化.
+  #
+  # See: Quincite::UI::TextLabel#initialize
+  #
   def initialize(id='', text='', x=0, y=0, *argv)
     super(id, text, x, y)
     @disable_color = [255,127,127,127]
@@ -9,22 +34,49 @@ class Quincite::UI::TextButton < Quincite::UI::TextLabel
     @hover_color = [255,255,223,127]
   end
 
+  ##############################################################################
+  #
+  # クリック時のイベントハンドラをセットする.
+  #
+  # Params:
+  #   - event_handler = Proc or block
+  #
   def onclick=(event_handler=Proc.new)
     add_event_handler(:mouse_left_push, event_handler)
   end
 
+  ##############################################################################
+  #
+  # イベントハンドラが無効化されているかどうか.
+  #
+  # Returns: boolean (true|false) ; 無効化されていれば true.
+  #
   def disable?
     @disable
   end
 
+  ##############################################################################
+  #
+  # イベントハンドラを有効化する.
+  #
   def enable
     @disable = false
   end
 
+  ##############################################################################
+  #
+  # イベントハンドラを無効化する.
+  #
   def disable
     @disable = true
   end
 
+  ##############################################################################
+  #
+  # 現在の文字色を取得する
+  #
+  # Returns: Array ([a, r, g, b])
+  #
   def color
     if disable?
       @color or @disable_color
