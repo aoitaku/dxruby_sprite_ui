dd06de301abbc0589a792e084dc5c4a562a3da86
aoitaku <aoitaku@gmail.com>
2015-02-20 23:17:59 +0900

    refactor text renderer

--- lib/dxruby_sprite_ui/components/text_renderer.rb
+++ lib/dxruby_sprite_ui/components/text_renderer.rb
@@ -8,38 +8,23 @@ module DXRuby::SpriteUI::TextRenderer
 
   def self.draw(x, y, drawable, context)
     text, params = *drawable.draw_params
-    alignment = if params[:text_align]
-      case params[:text_align]
-      when :center
-        -> text { x + (drawable.width - context.font.get_width(text)) / 2 }
-      when :right
-        -> text { x + (drawable.width - context.font.get_width(text)) }
-      else
-        -> text { x }
-      end
-    else
-      -> text { x }
+    target, font = context.target, context.font
+    align = text_align(params[:text_align], x, drawable.width, font)
+    draw_font = (target or Window).method(params[:aa] ? :draw_font_ex : :draw_font)
+    text.each_line.inject(y) do |y, line|
+      draw_font[align[line], y, line.chomp, font, params]
+      y + font.size
     end
-    if params[:aa]
-      text.each_line.with_index do |line, i|
-        (context.target or Window).draw_font_ex(
-          alignment[text],
-          y + context.font.size * i,
-          line.chomp,
-          context.font,
-          params
-        )
-      end
+  end
+
+  def self.text_align(align, x, width, font)
+    case align
+    when :center
+      -> text { x + (width - font.get_width(text)) / 2 }
+    when :right
+      -> text { x + (width - font.get_width(text)) }
     else
-      text.each_line.with_index do |line, i|
-        (context.target or Window).draw_font(
-          alignment[text],
-          y + context.font.size * i,
-          line.chomp,
-          context.font,
-          params
-        )
-      end
+      -> * { x }
     end
   end
 
