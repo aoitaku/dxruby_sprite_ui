30ed72e3cdb9d9ec0b6bb10b89df9a87575a2b19
aoitaku <aoitaku@gmail.com>
2015-02-21 00:21:01 +0900

    implement text align distributed by equal spacing per char (close #1)

--- lib/dxruby_sprite_ui/components/text_renderer.rb
+++ lib/dxruby_sprite_ui/components/text_renderer.rb
@@ -12,7 +12,27 @@ module DXRuby::SpriteUI::TextRenderer
     align = text_align(params[:text_align], x, drawable.width, font)
     draw_font = (target or Window).method(params[:aa] ? :draw_font_ex : :draw_font)
     text.each_line.inject(y) do |y, line|
-      draw_font[align[line], y, line.chomp, font, params]
+      if params[:text_align] == :fill
+        curr = line[0]
+        char_slice = line.each_char.slice_before {|e|
+          curr, prev = e, curr
+          /\s/ === e or not (e+prev).ascii_only?
+        }
+        chars = char_slice.reject {|char| char.reject {|c| /\s/ === c }.empty? }.to_a
+        if chars.size == 1
+          width = drawable.width - font.get_width(line)
+          draw_font[x + width / 2, y, line, font, params]
+        else
+          width = (drawable.width - drawable.padding * 2) - font.get_width(chars.join)
+          pad = width / (chars.size - 1)
+          chars.inject(x) do |x, char|
+            draw_font[x, y, char.join, font, params]
+            x + font.get_width(char.join) + pad
+          end
+        end
+      else
+        draw_font[align[line], y, line.chomp, font, params]
+      end
       y + font.size
     end
   end
