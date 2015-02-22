d38041e82b80de633db4df96e9a5b5fda3d88c33
aoitaku <aoitaku@gmail.com>
2015-02-23 03:38:46 +0900

    refactor text renderer

--- lib/dxruby_sprite_ui/components/text_renderer.rb
+++ lib/dxruby_sprite_ui/components/text_renderer.rb
@@ -53,27 +53,30 @@ module DXRuby::SpriteUI::TextRenderer
     draw_font = (target or Window).method(params[:aa] ? :draw_font_ex : :draw_font)
     text.each_line.inject(y) do |y, line|
       if params[:text_align] == :fill
-        curr = line[0]
-        char_slice = line.each_char.slice_before {|e|
-          curr, prev = e, curr
-          /\s/ === e or not (e+prev).ascii_only?
-        }
-        chars = char_slice.reject {|char| char.reject {|c| /\s/ === c }.empty? }.to_a
-        if chars.size == 1
-          width = drawable.width - font.get_width(line)
-          draw_font[x + width / 2, y, line, font, params]
-        else
-          width = (drawable.width - drawable.padding * 2) - font.get_width(chars.join)
-          pad = width / (chars.size - 1)
-          chars.inject(x) do |x, char|
-            draw_font[x, y, char.join, font, params]
-            x + font.get_width(char.join) + pad
-          end
-        end
+        align_fill(draw_font, line, drawable)
       else
         draw_font[align[line], y, line.chomp, font, params]
       end
       y + font.size
+    end
+  end
+
+  def self.align_fill(draw_font, text, drawable)
+    curr = text[0]
+    chars = text.each_char.slice_before {|e|
+      curr, prev = e, curr
+      /\s/ === e or not (e + prev).ascii_only?
+    }.reject {|char| char.reject {|c| /\s/ === c }.empty? }.to_a
+    if chars.size == 1
+      width = drawable.width - font.get_width(text)
+      draw_font[x + width / 2, y, text, font, params]
+    else
+      width = (drawable.width - drawable.padding * 2) - font.get_width(chars.join)
+      pad = width / (chars.size - 1)
+      chars.inject(x) do |x, char|
+        draw_font[x, y, char.join, font, params]
+        x + font.get_width(char.join) + pad
+      end
     end
   end
 
